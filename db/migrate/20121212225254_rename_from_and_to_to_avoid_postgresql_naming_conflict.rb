20daa3cdde3c24d70c009f60be71c1a13e05d459
Frank Rietta <frank@rietta.com>
2012-12-12 18:07:58 -0500

    Conditional renames on the fields
    Signed-off-by: Frank Rietta <frank@rietta.com>

--- db/migrate/20121212225254_rename_from_and_to_to_avoid_postgresql_naming_conflict.rb
+++ db/migrate/20121212225254_rename_from_and_to_to_avoid_postgresql_naming_conflict.rb
@@ -1,8 +1,13 @@
 class RenameFromAndToToAvoidPostgresqlNamingConflict < ActiveRecord::Migration
 
   def change
-    rename_column :refinery_calendar_events, :from, :from_at
-    rename_column :refinery_calendar_events, :to, :to_at
+    unless column_exists? :refinery_calendar_events, :from_at
+      rename_column :refinery_calendar_events, :from, :from_at
+    end
+    
+    unless column_exists? :refinery_calendar_events, :to_at
+      rename_column :refinery_calendar_events, :to, :to_at
+    end
   end
   
 end
